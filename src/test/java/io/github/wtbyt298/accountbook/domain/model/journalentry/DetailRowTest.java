18d17cb834c0ffd708662c26c78f5d218d80df4d
wtbyt298 <wtbyt@192.168.1.12>
2023-04-21 22:27:12 +0900

    Revert "明細行クラスのメンバを変更"
    This reverts commit 76e34929281a81a006d31136d0f438bca2bfb218.

--- src/test/java/io/github/wtbyt298/accountbook/domain/model/journalentry/DetailRowTest.java
+++ src/test/java/io/github/wtbyt298/accountbook/domain/model/journalentry/DetailRowTest.java
@@ -3,7 +3,11 @@ package io.github.wtbyt298.accountbook.domain.model.journalentry;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitleId;
+import io.github.wtbyt298.accountbook.domain.model.accounttitle.SubAccountTitleId;
+import io.github.wtbyt298.accountbook.domain.model.shared.Amount;
 import io.github.wtbyt298.accountbook.domain.model.shared.types.LoanType;
+import io.github.wtbyt298.accountbook.domain.model.shared.types.accountingtype.AccountingType;
 
 class DetailRowTest {
 	
@@ -12,9 +16,12 @@ class DetailRowTest {
 	
 	@BeforeAll
 	static void prepare() {
-		//貸借区分以外はここでは重要でないのでnullを渡している
-		debit = new DetailRow(null, null, LoanType.DEBIT, null);
-		credit = new DetailRow(null, null, LoanType.CREDIT, null);
+		//会計区分と貸借区分以外はここでは重要でないので使い回している
+		AccountTitleId id = AccountTitleId.valueOf("101");
+		SubAccountTitleId subId = SubAccountTitleId.valueOf("0");
+		Amount amount = Amount.valueOf(100);
+		debit = new DetailRow(id, subId, AccountingType.ASSETS, LoanType.DEBIT, amount);
+		credit = new DetailRow(id, subId, AccountingType.LIABILITIES, LoanType.CREDIT, amount);
 	}
 
 	@Test
@@ -27,6 +34,14 @@ class DetailRowTest {
 	void 貸借区分が貸方なら貸方明細と判定される() {
 		assertTrue(credit.isCredit());
 		assertFalse(credit.isDebit());
+	}
+
+	@Test
+	void 会計区分を使って組み合わせ可能かどうかを判断できる() {
+		//例えば、　借方：資産　貸方：負債　はOK
+		//その逆の、借方：負債　貸方：資産　もOK
+		assertTrue(debit.canCombinate(credit));
+		assertTrue(credit.canCombinate(debit));
 	}
 
 }
