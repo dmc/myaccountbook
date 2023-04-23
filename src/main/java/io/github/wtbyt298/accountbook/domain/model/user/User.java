417451406ab6456555bf650e12d0ed38c1a9fa25
wtbyt298 <wtbyt@192.168.1.12>
2023-04-23 22:22:26 +0900

    ドメイン層にユーザクラスを追加

---
+++ src/main/java/io/github/wtbyt298/accountbook/domain/model/user/User.java
@@ -0,0 +1,8 @@
+package io.github.wtbyt298.accountbook.domain.model.user;
+
+/**
+ * ユーザクラス
+ */
+public class User {
+
+}
---
+++ src/test/java/io/github/wtbyt298/accountbook/domain/model/user/UserTest.java
@@ -0,0 +1,14 @@
+package io.github.wtbyt298.accountbook.domain.model.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
