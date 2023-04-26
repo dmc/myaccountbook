22e35fc78203a4792dd9ab132fc4f01d4d07b871
wtbyt298 <wtbyt@192.168.1.12>
2023-04-26 13:07:22 +0900

    ユーザ登録のユースケースを実装

--- src/main/java/io/github/wtbyt298/accountbook/application/usecase/user/CreateUserUseCase.java
+++ src/main/java/io/github/wtbyt298/accountbook/application/usecase/user/CreateUserUseCase.java
@@ -1,6 +1,13 @@
 package io.github.wtbyt298.accountbook.application.usecase.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.github.wtbyt298.accountbook.domain.model.user.User;
+import io.github.wtbyt298.accountbook.domain.model.user.UserId;
+import io.github.wtbyt298.accountbook.domain.model.user.UserPassword;
+import io.github.wtbyt298.accountbook.domain.model.user.UserRepository;
 
 /**
  * ユーザ作成処理クラス
@@ -8,4 +15,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class CreateUserUseCase {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	/**
+	 * ユーザを作成する
+	 * @param ユーザ作成用のDTO
+	 */
+	@Transactional
+	public void execute(CreateUserCommand command) {
+		User user = createFromCommand(command);
+		if (userRepository.exists(user)) {
+			throw new IllegalArgumentException("既にユーザが存在しています。");
+		}
+		userRepository.save(user);
+	}
+	
+	/**
+	 * ユーザのインスタンスを組み立てる
+	 */
+	private User createFromCommand(CreateUserCommand command) {
+		UserId userId = UserId.valueOf(command.getId());
+		UserPassword userPassword = UserPassword.valueOf(command.getPassword());
+		return User.create(
+			userId, 
+			userPassword, 
+			command.getMailAddress()
+		);		
+	}
+	
 }
