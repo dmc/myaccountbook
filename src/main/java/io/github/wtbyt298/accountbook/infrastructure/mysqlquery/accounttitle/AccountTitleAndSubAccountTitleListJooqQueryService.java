091e73445687cbaf1a0d574ee72efae90e4f6dea
wtbyt298 <wtbyt@192.168.1.12>
2023-04-27 19:01:20 +0900

    勘定科目テーブルと補助科目テーブルをJOINするSQLを変更

--- src/main/java/io/github/wtbyt298/accountbook/infrastructure/mysqlquery/accounttitle/AccountTitleAndSubAccountTitleListJooqQueryService.java
+++ src/main/java/io/github/wtbyt298/accountbook/infrastructure/mysqlquery/accounttitle/AccountTitleAndSubAccountTitleListJooqQueryService.java
@@ -7,7 +7,6 @@ import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import static generated.Tables.*;
-import static org.jooq.impl.DSL.*;
 import io.github.wtbyt298.accountbook.application.query.model.accounttitle.AccountTitleAndSubAccountTitleDto;
 import io.github.wtbyt298.accountbook.application.query.service.accounttitle.AccountTitleAndSubAccountTitleListQueryService;
 
@@ -28,12 +27,13 @@ public class AccountTitleAndSubAccountTitleListJooqQueryService implements Accou
 	public List<AccountTitleAndSubAccountTitleDto> findAll() {
 		List<AccountTitleAndSubAccountTitleDto> list = new ArrayList<>();
 		Result<Record> result = jooq.select()
-									.from(ACCOUNTTITLES
-									.leftOuterJoin(SUB_ACCOUNTTITLES).on(ACCOUNTTITLES.ACCOUNTTITLE_ID.eq(SUB_ACCOUNTTITLES.ACCOUNTTITLE_ID))
-									.where(SUB_ACCOUNTTITLES.USER_ID.eq("TEST_USER"))) //TODO ログイン機能実装後に修正する
+									.from(ACCOUNTTITLES)
+									.leftOuterJoin(SUB_ACCOUNTTITLES).on(ACCOUNTTITLES.ACCOUNTTITLE_ID.eq(SUB_ACCOUNTTITLES.ACCOUNTTITLE_ID)
+									.and(SUB_ACCOUNTTITLES.USER_ID.eq("TEST_USER"))) //TODO ログイン機能実装後に修正する
+									.orderBy(ACCOUNTTITLES.ACCOUNTTITLE_ID, SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_ID)
 									.fetch();
 		if (result.isEmpty()) {
-			throw new RuntimeException("勘定科目と補助科目データの取得ができませんでした。");
+			throw new RuntimeException("勘定科目と補助科目のデータの取得ができませんでした。");
 		}
 		for (Record record : result) {
 			list.add(mapRecordToDto(record));
@@ -45,11 +45,20 @@ public class AccountTitleAndSubAccountTitleListJooqQueryService implements Accou
 	 * 取得したレコードを戻り値のDTOクラスに詰め替える
 	 */
 	private AccountTitleAndSubAccountTitleDto mapRecordToDto(Record record) {
-		return new AccountTitleAndSubAccountTitleDto(
+		if (record.get(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_ID) == null ||
+			record.get(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_NAME) == null	) {
+			return new AccountTitleAndSubAccountTitleDto(
+				record.get(ACCOUNTTITLES.ACCOUNTTITLE_ID), 
+				record.get(ACCOUNTTITLES.ACCOUNTTITLE_NAME),
+				"0", //補助科目が存在しない場合は
+				""   //補助科目ID："0"　補助科目名：""（空白）とする
+			);
+		}
+ 		return new AccountTitleAndSubAccountTitleDto(
 			record.get(ACCOUNTTITLES.ACCOUNTTITLE_ID), 
 			record.get(ACCOUNTTITLES.ACCOUNTTITLE_NAME), 
-			record.get(coalesce(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_ID, "0")), //補助科目が存在しない場合、IDは"0"とする
-			record.get(coalesce(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_NAME, "")) //補助科目が存在しない場合、補助科目名は空文字列とする
+			record.get(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_ID), 
+			record.get(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_NAME) 
 		);
 	}
 	
