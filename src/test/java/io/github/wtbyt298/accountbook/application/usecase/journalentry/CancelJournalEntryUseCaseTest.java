eaff434b3de3443659304f1da28737250a3321d9
wtbyt298 <wtbyt@192.168.1.12>
2023-04-28 22:12:12 +0900

    仕訳取消処理のテストを作成

--- src/test/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/CancelJournalEntryUseCaseTest.java
+++ src/test/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/CancelJournalEntryUseCaseTest.java
@@ -1,14 +1,57 @@
 package io.github.wtbyt298.accountbook.application.usecase.journalentry;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import io.github.wtbyt298.accountbook.domain.model.journalentry.EntryId;
+import io.github.wtbyt298.accountbook.domain.model.journalentry.JournalEntryRepository;
 
 class CancelJournalEntryUseCaseTest {
-
-	@Test
-	void test() {
-		fail("まだ実装されていません");
+	
+	@Mock
+	private JournalEntryRepository journalEntryRepository;
+	
+	@InjectMocks
+	private CancelJournalEntryUseCase cancelUseCase;
+	
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
+	@Test
+	void 仕訳IDを渡すと該当する仕訳が削除される() {
+		//given
+		ArgumentCaptor<EntryId> captor = ArgumentCaptor.forClass(EntryId.class);
+		EntryId entryId = EntryId.fromString("TEST");
+		when(journalEntryRepository.exists(entryId)).thenReturn(true); //仕訳IDに該当する仕訳は存在している
+		
+		//when
+		cancelUseCase.execute(entryId);
+		verify(journalEntryRepository).drop(captor.capture()); //リポジトリのdropメソッドに渡される値をキャプチャする
+		
+		//then
+		EntryId capturedId = captor.getValue();
+		assertEquals(entryId, capturedId);
+	}
+
+	@Test
+	void 仕訳IDに該当する仕訳が見つからない場合は例外発生() {
+		//given
+		EntryId entryId = EntryId.newInstance();
+		when(journalEntryRepository.exists(entryId)).thenReturn(false); //仕訳IDに該当する仕訳は存在しない
+		
+		//when
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> cancelUseCase.execute(entryId));
+		
+		//then
+		assertEquals("指定した仕訳は存在しません。", exception.getMessage());
+	}
+	
 }
