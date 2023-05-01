7528cc0aa935215746e634a47f9c29042cc5f794
wtbyt298 <wtbyt@192.168.1.12>
2023-05-01 10:59:25 +0900

    テストコードを変更

--- src/test/java/io/github/wtbyt298/accountbook/application/usecase/user/DisableUserUseCaseTest.java
+++ src/test/java/io/github/wtbyt298/accountbook/application/usecase/user/DisableUserUseCaseTest.java
@@ -1,6 +1,7 @@
 package io.github.wtbyt298.accountbook.application.usecase.user;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,11 +34,11 @@ class DisableUserUseCaseTest {
 	void 指定したユーザIDに該当するユーザを無効化できる() {
 		//given
 		ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
-		UserId userId = UserId.valueOf("TEST_USER");
-		when(userRepository.exists(userId)).thenReturn(true); //ユーザIDに該当するユーザは存在している
-		when(userRepository.findById(userId)).thenReturn(createActiveUser(userId)); 
+		when(userRepository.exists(any())).thenReturn(true); //ユーザIDに該当するユーザは存在している
+		when(userRepository.findById(any())).thenReturn(createActiveUser()); 
 		
 		//when
+		UserId userId = UserId.valueOf("TEST_USER");
 		disableUseCase.execute(userId);
 		verify(userRepository).update(captor.capture()); //リポジトリのupdateメソッドに渡される値をキャプチャする
 		
@@ -49,10 +50,10 @@ class DisableUserUseCaseTest {
 	@Test
 	void 指定したユーザIDに該当するユーザが見つからない場合は例外発生() {
 		//given
-		UserId userId = UserId.valueOf("TEST_USER");
-		when(userRepository.exists(userId)).thenReturn(false); //ユーザIDに該当するユーザは存在しない
+		when(userRepository.exists(any())).thenReturn(false); //ユーザIDに該当するユーザは存在しない
 		
 		//when
+		UserId userId = UserId.valueOf("TEST_USER");
 		Exception exception = assertThrows(IllegalArgumentException.class, () -> disableUseCase.execute(userId));
 		
 		//then
@@ -62,8 +63,9 @@ class DisableUserUseCaseTest {
 	/**
 	 * テスト用のユーザインスタンスを生成する
 	 */
-	private User createActiveUser(UserId userId) {
-		return User.reconstruct(userId, 
+	private User createActiveUser() {
+		return User.reconstruct(
+			UserId.valueOf("TEST_USER"), 
 			EncodedUserPassword.valueOf("TEST"), 
 			"test:example.com", 
 			UserStatus.ACTIVE
