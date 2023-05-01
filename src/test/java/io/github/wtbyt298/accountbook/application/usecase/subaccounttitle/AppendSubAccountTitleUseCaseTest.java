04b96c87138b700d489e571f7b3280eed57e7cd3
wtbyt298 <wtbyt@192.168.1.12>
2023-05-01 10:53:37 +0900

    テスト用オブジェクトを生成するクラスを追加、テストコードを変更

--- src/test/java/io/github/wtbyt298/accountbook/application/usecase/subaccounttitle/AppendSubAccountTitleUseCaseTest.java
+++ src/test/java/io/github/wtbyt298/accountbook/application/usecase/subaccounttitle/AppendSubAccountTitleUseCaseTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -13,13 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import io.github.wtbyt298.accountbook.application.shared.usersession.UserSession;
-import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitleId;
-import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitle;
 import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitleId;
-import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitleName;
 import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitleRepository;
 import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitles;
-import io.github.wtbyt298.accountbook.domain.model.user.UserId;
 
 class AppendSubAccountTitleUseCaseTest {
 	
@@ -41,10 +35,9 @@ class AppendSubAccountTitleUseCaseTest {
 	void 引数に渡したDTOをもとに作成された補助科目が追加される() {
 		//given
 		ArgumentCaptor<SubAccountTitles> captor = ArgumentCaptor.forClass(SubAccountTitles.class);
-		AccountTitleId parentId = AccountTitleId.valueOf("102");
 		//依存オブジェクトの設定
-		when(userSession.userId()).thenReturn(UserId.valueOf("TEST_USER"));
-		when(subAccountTitleRepository.findCollectionByParentId(parentId, userSession.userId())).thenReturn(prepareTestObject());
+		//既に「0：その他」「1：三菱UFJ銀行」という2つの補助科目が存在している
+		when(subAccountTitleRepository.findCollectionByParentId(any(), any())).thenReturn(TestObjectCreator.prepareTestObject());
 		
 		//when
 		AppendSubAccountTitleCommand command = new AppendSubAccountTitleCommand("102", "三井住友銀行");
@@ -56,27 +49,6 @@ class AppendSubAccountTitleUseCaseTest {
 		SubAccountTitles captured = captor.getValue();
 		assertEquals(3, captured.elements().size()); //要素数2 → 3
 		assertEquals("2：三井住友銀行", captured.find(added).toString());
-	}
-	
-	/**
-	 * テスト用の補助科目のコレクションオブジェクトを生成する
-	 * 初期値として以下の2つが追加されている
-	 * 「0：その他」
-	 * 「1：三菱UFJ銀行」
-	 */
-	private SubAccountTitles prepareTestObject() {
-		Map<SubAccountTitleId, SubAccountTitle> collection = new HashMap<>();
-		AccountTitleId parentId = AccountTitleId.valueOf("102");
-		collection.put(SubAccountTitleId.valueOf("0"), createSubAccountTitle("0", "その他"));
-		collection.put(SubAccountTitleId.valueOf("1"), createSubAccountTitle("1", "三菱UFJ銀行"));
-		return new SubAccountTitles(collection, parentId);
-	}
-	
-	private SubAccountTitle createSubAccountTitle(String id, String name) {
-		return new SubAccountTitle(
-			SubAccountTitleId.valueOf(id), 
-			SubAccountTitleName.valueOf(name)
-		);
 	}
 
 }
