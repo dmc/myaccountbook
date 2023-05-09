72b979571a225bb07bdd594bb7129a9ce045c0fb
wtbyt298 <wtbyt@192.168.1.12>
2023-05-09 23:53:45 +0900

    メンバ変数の型を変更

--- src/main/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/RegisterEntryDetailCommand.java
+++ src/main/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/RegisterEntryDetailCommand.java
@@ -1,6 +1,5 @@
 package io.github.wtbyt298.accountbook.application.usecase.journalentry;
 
-import io.github.wtbyt298.accountbook.domain.shared.types.LoanType;
 import lombok.Getter;
 
 /**
@@ -11,13 +10,13 @@ public class RegisterEntryDetailCommand {
 
 	private final String accountTitleId;    //勘定科目ID
 	private final String subAccountTitleId; //補助科目ID
-	private final LoanType detailLoanType;  //明細の貸借区分
+	private final String detailLoanType;  //明細の貸借区分
 	private final int amount;               //仕訳金額
 	
 	public RegisterEntryDetailCommand(String accountTitleId, String subAccountTitleId, String detailLoanType, int amount) {
 		this.accountTitleId = accountTitleId;
 		this.subAccountTitleId = subAccountTitleId;
-		this.detailLoanType = LoanType.valueOf(detailLoanType);
+		this.detailLoanType = detailLoanType;
 		this.amount = amount;
 	}
 	
