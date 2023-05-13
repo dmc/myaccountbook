d77c28b310ef820f6a511750b1f59dda5018f2b4
wtbyt298 <wtbyt@192.168.1.12>
2023-05-13 18:15:54 +0900

    メソッドの引数の型を変更

--- src/test/java/io/github/wtbyt298/accountbook/helper/testdatacreator/SubAccountTitleTestDataCreator.java
+++ src/test/java/io/github/wtbyt298/accountbook/helper/testdatacreator/SubAccountTitleTestDataCreator.java
@@ -4,7 +4,7 @@ import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import static generated.Tables.SUB_ACCOUNTTITLES;
-import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitle;
+import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitleId;
 import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitle;
 import io.github.wtbyt298.accountbook.domain.model.user.UserId;
 import io.github.wtbyt298.accountbook.helper.testfactory.SubAccountTitleTestFactory;
@@ -18,10 +18,10 @@ public class SubAccountTitleTestDataCreator {
 	@Autowired
 	private DSLContext jooq;
 	
-	public SubAccountTitle create(AccountTitle parent, String id, String name, UserId userId) {
+	public SubAccountTitle create(AccountTitleId parentId, String id, String name, UserId userId) {
 		jooq.insertInto(SUB_ACCOUNTTITLES)
 			.set(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_ID, id)
-			.set(SUB_ACCOUNTTITLES.ACCOUNTTITLE_ID, parent.id().value())
+			.set(SUB_ACCOUNTTITLES.ACCOUNTTITLE_ID, parentId.value())
 			.set(SUB_ACCOUNTTITLES.USER_ID, userId.value())
 			.set(SUB_ACCOUNTTITLES.SUB_ACCOUNTTITLE_NAME, name)
 			.execute();
