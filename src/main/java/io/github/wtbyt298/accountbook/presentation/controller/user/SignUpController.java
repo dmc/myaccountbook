fe30a3e0017d8a053fe45c45e87c377153908ab0
wtbyt298 <wtbyt@192.168.1.12>
2023-05-15 21:35:36 +0900

    ユーザ登録処理のコントローラに自動ログイン処理を追加

--- src/main/java/io/github/wtbyt298/accountbook/presentation/controller/user/SignUpController.java
+++ src/main/java/io/github/wtbyt298/accountbook/presentation/controller/user/SignUpController.java
@@ -1,7 +1,10 @@
 package io.github.wtbyt298.accountbook.presentation.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import io.github.wtbyt298.accountbook.application.usecase.user.CreateUserCommand;
 import io.github.wtbyt298.accountbook.application.usecase.user.CreateUserUseCase;
 import io.github.wtbyt298.accountbook.presentation.request.user.CreateUserParam;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 /**
@@ -18,24 +23,50 @@ import jakarta.validation.Valid;
 public class SignUpController {
 
 	@Autowired
-	private CreateUserUseCase createUseCase;
-	
-	@GetMapping("/signup")
-	public String signUp() {
-		return "/signup";
+	private CreateUserUseCase createUserUseCase;
+
+	@GetMapping("/user/signup")
+	public String load(@ModelAttribute("userParam") CreateUserParam param) {
+		return "/user/signup";
 	}
 	
 	/**
-	 * フォームに入力した内容でユーザを登録する
+	 * ユーザを作成する
 	 */
 	@PostMapping("/signup")
-	public String register(@Valid @ModelAttribute("userParam") CreateUserParam param, BindingResult result) {
+	public String signUp(@Valid @ModelAttribute("userParam") CreateUserParam param, BindingResult result, Model model, HttpServletRequest request) {
 		if (result.hasErrors()) {
-			return "/signup";
+			return "/user/signup";
 		}
-		CreateUserCommand command = new CreateUserCommand(param.getId(), param.getPassword(), param.getMailAddress());
-		createUseCase.execute(command);
-		return "登録完了画面";
+		try {
+			CreateUserCommand command = new CreateUserCommand(
+				param.getId(), 
+				param.getPassword(), 
+				param.getMailAddress()
+			);
+			createUserUseCase.execute(command);
+			autoLogin(param.getId(), param.getPassword(), request);
+			return "redirect:/user/home";
+		} catch (RuntimeException e) {
+			model.addAttribute("errorMessage", e.getMessage());
+			return "/user/signup";
+		}
+	}
+	
+	/**
+	 * 自動でログイン処理を行う
+	 */
+	private void autoLogin(String userId, String password, HttpServletRequest request) {
+		//既にユーザがログイン済みの場合は、一旦ログアウトさせる
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication.isAuthenticated()) {
+			SecurityContextHolder.clearContext();
+		}
+		try {
+			request.login(userId, password);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
