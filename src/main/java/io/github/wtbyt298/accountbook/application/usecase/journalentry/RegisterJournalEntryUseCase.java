7b8f1986f2739a90814e9eb0140288fba462e8da
wtbyt298 <wtbyt@192.168.1.12>
2023-05-21 00:34:41 +0900

    例外の型を変更

--- src/main/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/RegisterJournalEntryUseCase.java
+++ src/main/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/RegisterJournalEntryUseCase.java
@@ -3,14 +3,13 @@ package io.github.wtbyt298.accountbook.application.usecase.journalentry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import io.github.wtbyt298.accountbook.application.shared.exception.ApplicationException;
 import io.github.wtbyt298.accountbook.application.shared.usersession.UserSession;
 import io.github.wtbyt298.accountbook.application.usecase.shared.AccountBalanceUpdator;
 import io.github.wtbyt298.accountbook.domain.model.journalentry.*;
 import io.github.wtbyt298.accountbook.domain.model.user.UserId;
 import io.github.wtbyt298.accountbook.domain.service.JournalEntryFactory;
 import io.github.wtbyt298.accountbook.domain.service.JournalEntrySpecification;
+import io.github.wtbyt298.accountbook.domain.shared.exception.DomainException;
 
 /**
  * 仕訳登録処理クラス
@@ -39,7 +38,7 @@ public class RegisterJournalEntryUseCase {
 		UserId userId = userSession.userId();
 		JournalEntry entry = journalEntryFactory.create(command, userId);
 		if (! journalEntrySpecification.isSatisfied(entry)) {
-			throw new ApplicationException("仕訳明細の貸借組み合わせが正しくありません。");
+			throw new DomainException("明細の貸借組み合わせが正しくありません。");
 		}
 		//残高更新サービスに引き渡し、リポジトリに保存する
 		accountBalanceUpdator.execute(entry, userId);
