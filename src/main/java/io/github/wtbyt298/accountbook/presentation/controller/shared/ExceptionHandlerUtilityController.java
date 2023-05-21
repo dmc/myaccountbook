7cea70a87b58daaebcc36ef4476873a45f5dcd3a
wtbyt298 <wtbyt@192.168.1.12>
2023-05-21 16:31:51 +0900

    クラス名、パッケージを変更

---
+++ src/main/java/io/github/wtbyt298/accountbook/presentation/controller/shared/ExceptionHandlerUtilityController.java
@@ -0,0 +1,12 @@
+package io.github.wtbyt298.accountbook.presentation.controller.shared;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * 共通の例外処理用のコントローラクラス
+ * 各層で投げられた例外を処理する
+ */
+@ControllerAdvice
+public class ExceptionHandlerUtilityController {
+
+}
