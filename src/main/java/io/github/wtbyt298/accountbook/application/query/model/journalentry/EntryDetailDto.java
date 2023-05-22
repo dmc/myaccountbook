79b4c34f8bcd02d0048a6de67cbd3217590e9e2c
wtbyt298 <wtbyt@192.168.1.12>
2023-05-22 16:24:42 +0900

    フィールドを追加

--- src/main/java/io/github/wtbyt298/accountbook/application/query/model/journalentry/EntryDetailDto.java
+++ src/main/java/io/github/wtbyt298/accountbook/application/query/model/journalentry/EntryDetailDto.java
@@ -8,14 +8,18 @@ import lombok.Getter;
  */
 @Getter
 public class EntryDetailDto {
-
+	
+	private final String accountTitleId;
 	private final String accountTitleName;
+	private final String subAccountTitleId;
 	private final String subAccountTitleName;
 	private final LoanType detailLoanType;
 	private final int amount;
 	
-	public EntryDetailDto(String accountTitleName, String subAccountTitleName, String detailLoanType, int amount) {
+	public EntryDetailDto(String accountTitleId, String accountTitleName, String subAccountTitleId, String subAccountTitleName, String detailLoanType, int amount) {
+		this.accountTitleId = accountTitleId;
 		this.accountTitleName = accountTitleName;
+		this.subAccountTitleId = subAccountTitleId;
 		this.subAccountTitleName = subAccountTitleName;
 		this.detailLoanType = LoanType.valueOf(detailLoanType);
 		this.amount = amount;
