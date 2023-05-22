2db3ed0c82a63903ca1502fde67ae1cdf28383fe
wtbyt298 <wtbyt@192.168.1.12>
2023-05-22 16:24:56 +0900

    ローカル変数名を変更

--- src/main/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/CancelJournalEntryUseCase.java
+++ src/main/java/io/github/wtbyt298/accountbook/application/usecase/journalentry/CancelJournalEntryUseCase.java
@@ -32,10 +32,10 @@ public class CancelJournalEntryUseCase {
 		if (! journalEntryRepository.exists(entryId)) {
 			throw new ApplicationException("指定した仕訳は存在しません。");
 		}
-		JournalEntry journalEntry = journalEntryRepository.findById(entryId);
+		JournalEntry entry = journalEntryRepository.findById(entryId);
 		UserId userId = userSession.userId();
 		//貸借を入れ替えた仕訳を生成し、残高更新サービスに引き渡す
-		JournalEntry reversingEntry = journalEntry.toReversingJournalEntry();
+		JournalEntry reversingEntry = entry.toReversingJournalEntry();
 		accountBalanceUpdator.execute(reversingEntry, userId);
 		//元の仕訳を削除する
 		journalEntryRepository.drop(entryId);
