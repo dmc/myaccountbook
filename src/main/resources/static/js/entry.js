ca3a63173b466cf3e7ea44ecfe711a977060a91a
wtbyt298 <wtbyt@192.168.1.12>
2023-05-23 00:43:56 +0900

    javascriptで合計金額を計算、表示できるようにした

--- src/main/resources/static/js/entry.js
+++ src/main/resources/static/js/entry.js
@@ -1,4 +1,17 @@
 /**
+ * 現在の年月日を表す文字列を作成する
+ */
+window.addEventListener('load', function() {
+	var now = new Date();
+    var yyyy = now.getFullYear();
+    var mm = ('0' + (now.getMonth() + 1)).slice(-2);
+    var dd = ('0' + now.getDate()).slice(-2);
+    
+    var form_date = document.getElementById('form-date');
+	form_date.value = yyyy + '-' + mm + '-' + dd;
+});
+
+/**
  * 削除ボタン押下時に確認メッセージを表示する
  */
 function showConfirmMessage() {
@@ -16,4 +29,41 @@ function showConfirmMessage() {
  */
 function activate() {
 	document.getElementById('form-register').onsubmit = function() {return true};
+}
+
+/**
+ * 画面表示時に合計金額を表示する
+ */
+window.addEventListener('load', function() {
+	showTotalAmount('debit');
+	showTotalAmount('credit');
+});
+
+/**
+ * 合計金額を表示用の文字列として取得する
+ */
+function showTotalAmount(type) {
+	switch (type) {
+		case 'debit':
+			document.getElementById('debit-total').innerText = '借方合計：' + calculate(type) + '円';
+			break;
+		case 'credit':
+			document.getElementById('credit-total').innerText = '貸方合計：' + calculate(type) + '円';
+			break;
+	}
+}
+
+/**
+ * 合計金額を再計算する
+ */
+function calculate(type) {
+	var total = 0;
+	var className = '.' + type + '-amount';
+	var forms = document.querySelectorAll(className);
+	for (var i = 0; i < forms.length; i++) {
+		var adding = forms[i].value;
+		if (adding == '') continue;
+		total += parseInt(adding);
+	}
+	return total;
 }
