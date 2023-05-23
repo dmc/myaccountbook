fafbe685a3aaf29ceb3f26ce0e10b1d66d351aae
wtbyt298 <wtbyt@192.168.1.12>
2023-05-23 21:47:52 +0900

    補助科目追加・名前変更処理のコントローラクラスを追加

---
+++ src/main/java/io/github/wtbyt298/accountbook/presentation/controller/accounttitle/AccountTitleDetailController.java
@@ -0,0 +1,78 @@
+package io.github.wtbyt298.accountbook.presentation.controller.accounttitle;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import io.github.wtbyt298.accountbook.application.shared.usersession.UserSession;
+import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitle;
+import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitleId;
+import io.github.wtbyt298.accountbook.domain.model.accounttitle.AccountTitleRepository;
+import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitle;
+import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitleRepository;
+import io.github.wtbyt298.accountbook.domain.model.subaccounttitle.SubAccountTitles;
+import io.github.wtbyt298.accountbook.presentation.response.accounttitle.AccountTitleViewModel;
+import io.github.wtbyt298.accountbook.presentation.response.accounttitle.SubAccountTitleViewModel;
+import io.github.wtbyt298.accountbook.presentation.shared.usersession.UserSessionProvider;
+
+/**
+ * 勘定科目詳細画面のコントローラクラス
+ */
+@Controller
+public class AccountTitleDetailController {
+	
+	//例外的にコントローラからリポジトリへの参照を許容している
+	
+	@Autowired
+	private AccountTitleRepository accountTitleRepository;
+	
+	@Autowired
+	private SubAccountTitleRepository subAccountTitleRepository;
+	
+	@Autowired
+	private UserSessionProvider userSessionProvider;
+
+	@GetMapping("/accounttitle/detail/{id}")
+	public String load(@PathVariable String id, Model model) {
+		AccountTitleId parentId = AccountTitleId.valueOf(id);
+		AccountTitleViewModel parentViewModel = createAccountTitleViewModel(parentId);
+		model.addAttribute("accountTitle", parentViewModel);
+		List<SubAccountTitleViewModel> subViewModels = createSubAccountTitleViewModels(parentId);
+		model.addAttribute("subAccountTitles", subViewModels);
+		if (subViewModels.isEmpty()) {
+			model.addAttribute("empty", "補助科目はまだ作成されていません。");
+		}
+		return "/accounttitle/detail";
+	}
+	
+	/**
+	 * 勘定科目のビューモデルを取得する
+	 */
+	private AccountTitleViewModel createAccountTitleViewModel(AccountTitleId parentId) {
+		AccountTitle accountTitle = accountTitleRepository.findById(parentId);
+		return new AccountTitleViewModel(
+			accountTitle.id().value(), 
+			accountTitle.name().value(), 
+			accountTitle.accountingType().lavel(), 
+			accountTitle.accountingType().loanType().toString(), 
+			accountTitle.accountingType().summaryType().toString()
+		);
+	}
+	
+	/**
+	 * 補助科目名のリストを取得する
+	 */
+	public List<SubAccountTitleViewModel> createSubAccountTitleViewModels(AccountTitleId parentId) {
+		UserSession userSession = userSessionProvider.getUserSession();
+		SubAccountTitles subAccountTitles = subAccountTitleRepository.findCollectionByParentId(parentId, userSession.userId());
+		List<SubAccountTitleViewModel> subNames = new ArrayList<>();
+		for (SubAccountTitle each : subAccountTitles.elements().values()) {
+			subNames.add(new SubAccountTitleViewModel(each.id().value(), each.name().value()));
+		}
+		return subNames;
+	}
+	
+}
---
+++ src/main/java/io/github/wtbyt298/accountbook/presentation/controller/subaccounttitle/RenameSubAccountTitleController.java
@@ -0,0 +1,35 @@
+package io.github.wtbyt298.accountbook.presentation.controller.subaccounttitle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import io.github.wtbyt298.accountbook.application.shared.usersession.UserSession;
+import io.github.wtbyt298.accountbook.application.usecase.subaccounttitle.RenameSubAccountTitleCommand;
+import io.github.wtbyt298.accountbook.application.usecase.subaccounttitle.RenameSubAccountTitleUseCase;
+import io.github.wtbyt298.accountbook.presentation.shared.usersession.UserSessionProvider;
+
+/**
+ * 補助科目名変更処理のコントローラクラス
+ */
+@Controller
+public class RenameSubAccountTitleController {
+
+	@Autowired
+	private RenameSubAccountTitleUseCase renameSubAccountTitleUseCase;
+	
+	@Autowired
+	private UserSessionProvider userSessionProvider;
+	
+	/**
+	 * 補助科目名を変更する
+	 */
+	@PostMapping("/accounttitle/edit")
+	public String rename(@RequestParam String parentId, @RequestParam String subId, @RequestParam String newName) {
+		RenameSubAccountTitleCommand command = new RenameSubAccountTitleCommand(parentId, subId, newName);
+		UserSession userSession = userSessionProvider.getUserSession();
+		renameSubAccountTitleUseCase.execute(command, userSession);
+		return "redirect:/accounttitle/detail/" + parentId;
+	}
+	
+}
