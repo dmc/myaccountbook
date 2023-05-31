7af3b7a17479e257cb6b71085464aba33de8b7ad
wtbyt298 <wtbyt@192.168.1.12>
2023-06-01 00:22:58 +0900

    テストコードを追加

---
+++ src/test/java/io/github/wtbyt298/accountbook/presentation/controller/journalentry/FetchListOfJournalEntryControllerTest.java
@@ -0,0 +1,98 @@
+package io.github.wtbyt298.accountbook.presentation.controller.journalentry;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import java.time.YearMonth;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import io.github.wtbyt298.accountbook.application.query.service.journalentry.FetchJournalEntryDataQueryService;
+import io.github.wtbyt298.accountbook.application.query.service.journalentry.JournalEntryOrderKey;
+import io.github.wtbyt298.accountbook.application.shared.usersession.UserSession;
+import io.github.wtbyt298.accountbook.domain.model.user.UserId;
+import io.github.wtbyt298.accountbook.infrastructure.shared.exception.RecordNotFoundException;
+import io.github.wtbyt298.accountbook.presentation.shared.usersession.UserSessionProvider;
+
+@SpringBootTest
+class FetchListOfJournalEntryControllerTest {
+
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private FetchJournalEntryDataQueryService fetchJournalEntryDataQueryService;
+	
+	@MockBean
+	private UserSessionProvider userSessionProvider;
+	
+	@Autowired
+	private FilterChainProxy springSecurityFilterChain;
+	
+	@InjectMocks
+	@Autowired
+	private FetchListOfJournalEntryController fetchListOfJournalEntryController;
+	
+	@BeforeEach
+	void setUp() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setSuffix(".html");
+        mockMvc = MockMvcBuilders.standaloneSetup(fetchListOfJournalEntryController)
+			.apply(springSecurity(springSecurityFilterChain))
+			.setViewResolvers(viewResolver)
+			.build();
+        
+		UserSession userSession = new UserSession() {
+			@Override
+			public UserId userId() {
+				return UserId.valueOf("TEST_USER");
+			}
+		};
+		when(userSessionProvider.getUserSession()).thenReturn(userSession);
+	}
+	
+	@Test
+	@WithMockUser
+	void 年月を指定してGETリクエストを送信すると仕訳一覧画面を表示する() throws Exception {
+		//when:
+		mockMvc.perform(get("/entry/entries/2023-05").param("selectedYearMonth", "2023-05"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("entries"))
+			.andExpect(model().attributeDoesNotExist("errorMessage"))
+			.andExpect(view().name("/entry/entries"));
+	}
+	
+	@Test
+	@WithMockUser
+	void RecordNotFoundExceptionが発生した場合はエラーメッセージを表示する() throws Exception {
+		//given:クエリサービスのfetchAllメソッドを実行するとRecordNotFoundExceptionが発生する
+		Exception exception = new RecordNotFoundException("");
+		when(fetchJournalEntryDataQueryService.fetchAll(YearMonth.of(2023, 5), JournalEntryOrderKey.DEAL_DATE, userSessionProvider.getUserSession().userId())).thenThrow(exception);
+		
+		//when:
+		mockMvc.perform(get("/entry/entries/2023-05").param("selectedYearMonth", "2023-05"))
+			.andExpect(status().isOk())
+			.andExpect(model().attribute("errorMessage", exception.getMessage()))
+			.andExpect(view().name("/entry/entries"));
+	}
+	
+	@Test
+	void 権限なしでアクセスするとログイン画面にリダイレクトされる() throws Exception {
+		//when:
+		mockMvc.perform(get("/entry/entries/2023-05"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrlPattern("**/user/login"));
+	}
+
+}
