0d9dda564850bf8388250b91f71d4ecfbc29e30b
wtbyt298 <wtbyt@192.168.1.12>
2023-06-02 16:47:24 +0900

    javascriptファイルを編集

--- src/main/resources/static/js/entry.js
+++ src/main/resources/static/js/entry.js
@@ -1,17 +1,4 @@
 /**
- * 現在の年月日を表す文字列を作成する
- */
-window.addEventListener('load', function() {
-	var now = new Date();
-    var yyyy = now.getFullYear();
-    var mm = ('0' + (now.getMonth() + 1)).slice(-2);
-    var dd = ('0' + now.getDate()).slice(-2);
-    
-    var form_date = document.getElementById('form-date');
-	form_date.value = yyyy + '-' + mm + '-' + dd;
-});
-
-/**
  * 削除ボタン押下時に確認メッセージを表示する
  */
 function showConfirmMessage() {
